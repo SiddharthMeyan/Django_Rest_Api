af3a53616c2c8bdb4fed9bf1e9f8e182f13405c2
SiddharthMeyan <siddharthmeyan@gmail.com>
2021-05-26 16:44:15 +0530

    created Tasklist view(to see task)

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
 
-# Create your views here.
+
+@api_view(['GET'])
+def tasklist(request):
+    task = Task.objects.all()
+    serializer = TaskSerializer(task, many=True)
+    return Response(serializer.data)
