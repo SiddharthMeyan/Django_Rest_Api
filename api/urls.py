3f329f5a014eade92f58c775fc62ff83ffdf3d2f
SiddharthMeyan <siddharthmeyan@gmail.com>
2021-05-27 15:35:11 +0530

    all CRUD functionality of our API

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,8 @@ from . import views
 urlpatterns = [
     path('task-list/', views.tasklist, name='tasklist'),
     path('task-details/<str:pk>', views.taskDetails, name='taskDetails'),
+    path('task-create', views.taskCreate, name='taskCreate'),
+    path('task-delete/<str:pk>', views.taskDelete, name='taskDelete'),
+    path('task-update/<str:pk>', views.taskUpdate, name='taskUpdate'),
 
 ]
--- api/views.py
+++ api/views.py
@@ -18,3 +18,27 @@ def taskDetails(request, pk):
     task = Task.objects.get(id=pk)
     serializer = TaskSerializer(task, many=False)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    return Response("no no")
+
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
